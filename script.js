0e1392a9228d70e3300c7b995f18b2a727d5f6bd
Pramod Soni <pramodsoni41@gmail.com>
2026-04-27 14:17:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -280,7 +280,22 @@ function closePasswordModal() {
   $("passwordModal").style.display = "none";
 }
 
+function goToQuiz() {
 
+  const roll = localStorage.getItem("roll");
+  const name = localStorage.getItem("name");
+
+  if (!roll) {
+    alert("Session expired. Please login again.");
+    window.location.href = "index.html";
+    return;
+  }
+
+  // Pass data via URL
+  const url = `quiz.html?roll=${encodeURIComponent(roll)}&name=${encodeURIComponent(name)}`;
+
+  window.location.href = url;
+}
 // ==========================
 // CHANGE PASSWORD
 // ==========================
@@ -353,22 +368,7 @@ window.onclick = function(e) {
     modal.style.display = "none";
   }
 };
-function goToQuiz() {
 
-  const roll = localStorage.getItem("roll");
-  const name = localStorage.getItem("name");
-
-  if (!roll) {
-    alert("Session expired. Please login again.");
-    window.location.href = "index.html";
-    return;
-  }
-
-  // Pass data via URL
-  const url = `quiz.html?roll=${encodeURIComponent(roll)}&name=${encodeURIComponent(name)}`;
-
-  window.location.href = url;
-}
 
 // ==========================
 // INIT
