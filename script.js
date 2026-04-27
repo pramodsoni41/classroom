3c6ec6018f101b35c25f33d2c16da28668b5d709
Pramod Soni <pramodsoni41@gmail.com>
2026-04-27 14:27:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -292,7 +292,7 @@ function goToQuiz() {
   }
 
   // Simple redirect (no need to pass URL params)
-  window.location.href = "pramodsoni41.github.io/Quiz/";
+  window.location.href = "https://pramodsoni41.github.io/Quiz/";
 }
 // ==========================
 // CHANGE PASSWORD
